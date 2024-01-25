92748ab11d1ba24c1def269dc8095bec06d7e12b
jonathanlukass <jhonatanlukas05@gmail.com>
2024-01-25 22:05:10 +0800

    adding new

---
+++ members.js
@@ -0,0 +1,34 @@
+const members = [
+    {
+        "id": 1,
+        "nim": "105022110080",
+        "nama": "Lukas, Jonathan",
+        "nomor_telp": "0897-9520-945"
+    },
+    {
+        "id": 2,
+        "nim": "-",
+        "nama": "Tengker, Joshua",
+        "nomor_telp": "0821-9247-1841"
+    },
+    {
+        "id": 3,
+        "nim": "-",
+        "nama": "Timbuleng, Schrievend",
+        "nomor_telp": "0896-2636-8687"
+    },
+    {
+        "id": 4,
+        "nim": "-",
+        "nama": "Manoppo, Timothy",
+        "nomor_telp": "0 895-3750-12000"
+    },
+    {
+        "id": 5,
+        "nim": "-",
+        "nama": "Politon, Vrandly",
+        "nomor_telp": "0813-5072-5166"
+    }
+];
+
+module.exports = members;
